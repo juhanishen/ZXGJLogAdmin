b873537f194997f51f1e29dc988083e794745098
juhanishen <juhanishen@gmail.com>
2015-02-19 14:29:26 +0200

    Image Map is enhanced.

--- src/com/zxgj/logadmin/client/webapp.java
+++ src/com/zxgj/logadmin/client/webapp.java
@@ -4,7 +4,6 @@ import com.zxgj.logadmin.client.zxgj.AreaWidget;
 import com.zxgj.logadmin.client.zxgj.MapWidget;
 import com.zxgj.logadmin.client.zxgj.ZXGJEAPCommentPanel;
 import com.zxgj.logadmin.client.zxgj.ZXGJEAPEventPanel;
-import com.zxgj.logadmin.client.zxgj.ZXGJEPSMainPanel;
 import com.zxgj.logadmin.client.zxgj.ZXGJKnowledgePanel;
 import com.zxgj.logadmin.client.zxgj.ZXGJSECMainPanel;
 import com.zxgj.logadmin.client.zxgj.ZXGJSearchPanel;
@@ -14,9 +13,9 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.LoadListener;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -28,19 +27,8 @@ import com.google.gwt.user.client.ui.Widget;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class webapp implements EntryPoint {
-//  /**
-//   * The message displayed to the user when the server cannot be reached or
-//   * returns an error.
-//   */
-//  private static final String SERVER_ERROR = "An error occurred while "
-//      + "attempting to contact the server. Please check your network "
-//      + "connection and try again.";
-//
-//  /**
-//   * Create a remote service proxy to talk to the server-side Greeting service.
-//   */
-//  private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
+  final Image image = new Image(); 
   /**
    * This is the entry point method.
    */
@@ -96,24 +84,23 @@ public class webapp implements EntryPoint {
    }
 
 private void createImageMap() {
-	final Image image = new Image(); 
     image.setUrl("nodeTopology.gif"); 
     RootPanel.get("imagemap").add(image); 
 
     // CREATE SHAPES 
     AreaWidget node6 = new AreaWidget("rect", "534, 157, 670, 214", "Node6", 
-                new SolarSystemCommand("Node6")); 
+                new SolarSystemCommand("Node6","nodeTopologyNode6Selected.gif")); 
     AreaWidget node5 = new AreaWidget("rect", "325, 158, 469, 217", "Node5", 
-                new SolarSystemCommand("Node5")); 
+                new SolarSystemCommand("Node5","nodeTopologyNode5Selected.gif")); 
     
     AreaWidget node4 = new AreaWidget("rect", "115, 168, 220, 219", "Node4", 
-            new SolarSystemCommand("Node4")); 
+            new SolarSystemCommand("Node4","nodeTopologyNode4Selected.gif")); 
     AreaWidget node3 = new AreaWidget("rect", "539, 16, 668, 76", "Node3", 
-            new SolarSystemCommand("Node3")); 
+            new SolarSystemCommand("Node3","nodeTopologyNode3Selected.gif")); 
     AreaWidget node2 = new AreaWidget("rect", "324, 14, 454, 76", "Node2", 
-            new SolarSystemCommand("Node2")); 
+            new SolarSystemCommand("Node2","nodeTopologyNode2Selected.gif")); 
     AreaWidget node1 = new AreaWidget("rect", "106, 16, 219, 79", "Node1", 
-            new SolarSystemCommand("Node1")); 
+            new SolarSystemCommand("Node1","nodeTopologyNode1Selected.gif")); 
 //    AreaWidget venus = new AreaWidget("circle", "124 58 8", "Venus", 
 //                new SolarSystemCommand("Venus")); 
     // CREATE MAP 
@@ -142,21 +129,28 @@ private void createImageMap() {
 
 class SolarSystemCommand implements Command { 
     String cell; 
+    String gifFileName;
 
-    public SolarSystemCommand(String cell) { 
-        this.cell = cell; 
+    public SolarSystemCommand(String cell,String gifFileName) { 
+        this.cell = cell;
+        this.gifFileName=gifFileName;
     } 
 
     public void execute() { 
-//        Window.alert("You've clicked: " +cell+ ", this will begin the process to collect "+cell+" logs"); 
+    	image.setUrl(gifFileName); 
     	final DialogBox dialogBox = new DialogBox();
     	dialogBox.setWidth("500px");
     	dialogBox.setHeight("400px");
-    	dialogBox.setTitle("This is "+cell+" remote console");
-    	dialogBox.setText("Computer: "+cell+ "'s remote terminal is opened: Here one could manipulate remote computer logs,"
-    			+ "and do some remote operation, for example check memory and cpu usage...");
-    	Button example = new Button("Check node Memory Usage");
-    	example.addClickHandler(new ClickHandler() {
+    	dialogBox.setAnimationEnabled(true);
+    	
+
+    	VerticalPanel dialogVPanel = new VerticalPanel();
+    	dialogVPanel.add(new HTML("<br/>This is "+cell+" remote console<br/>"));
+		dialogVPanel.add(new HTML("<br/>Computer: "+cell+ "'s remote terminal is opened: Here one could manipulate remote computer logs</br>"));
+		dialogVPanel.add(new HTML("<br/>and do some remote operation, for example check memory and cpu usage...</br/>"));
+
+		Button example = new Button("Check node Memory Usage"); 	
+        example.addClickHandler(new ClickHandler() {
           public void onClick(ClickEvent event) {
              dialogBox.setText("Here would be remote computer/node memory usage");
           }
@@ -165,12 +159,13 @@ class SolarSystemCommand implements Command {
     	close.addClickHandler(new ClickHandler() {
           public void onClick(ClickEvent event) {
              dialogBox.hide();
+             image.setUrl("nodeTopology.gif"); 
           }
         });
-    	VerticalPanel vp = new VerticalPanel();
-    	vp.add(example);
-    	vp.add(close);
-        dialogBox.setWidget(vp);
+    	dialogVPanel.add(example);
+    	dialogVPanel.add(close);
+  
+        dialogBox.setWidget(dialogVPanel);
         RootPanel.get("dialogbox").add(dialogBox);
         dialogBox.show();
     } 
